899bdc3dfe45c2c62b213169ce67b6b0128679cd
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-07-09 19:30:56 -0400

    refactor(client): use ox_lib's dialog interface to show form

--- client/main.lua
+++ client/main.lua
@@ -1,72 +1,72 @@
-local loadingScreenFinished = false
-local ready = false
-local guiEnabled = false
-local timecycleModifier = "hud_def_blur"
+local isLoadingScreenFinished = false
 
-RegisterNetEvent('esx_identity:alreadyRegistered', function()
-    while not loadingScreenFinished do Wait(100) end
-    TriggerEvent('esx_skin:playerRegistered')
+RegisterNetEvent("esx_identity:alreadyRegistered", function()
+    while not isLoadingScreenFinished do Wait(100) end
+
+    TriggerEvent("esx_skin:playerRegistered")
 end)
 
-RegisterNetEvent('esx_identity:setPlayerData', function(data)
-    SetTimeout(1, function()
-        ESX.SetPlayerData("name", ('%s %s'):format(data.firstName, data.lastName))
-        ESX.SetPlayerData('firstName', data.firstName)
-        ESX.SetPlayerData('lastName', data.lastName)
-        ESX.SetPlayerData('dateofbirth', data.dateOfBirth)
-        ESX.SetPlayerData('sex', data.sex)
-        ESX.SetPlayerData('height', data.height)
-    end)
+RegisterNetEvent("esx_identity:setPlayerData", function(data)
+    if type(data) ~= "table" then return end
+
+    Wait(10)
+
+    ESX.SetPlayerData("name", ("%s %s"):format(data.firstName, data.lastName))
+    ESX.SetPlayerData("firstName", data.firstName)
+    ESX.SetPlayerData("lastName", data.lastName)
+    ESX.SetPlayerData("dateofbirth", data.dateOfBirth)
+    ESX.SetPlayerData("sex", data.sex)
+    ESX.SetPlayerData("height", data.height)
 end)
 
-AddEventHandler('esx:loadingScreenOff', function()
-    loadingScreenFinished = true
+AddEventHandler("esx:loadingScreenOff", function()
+    isLoadingScreenFinished = true
 end)
 
-RegisterNUICallback('ready', function(data, cb)
-    ready = true
-    cb(1)
-end)
+if Config.UseDeferrals then return end
 
-if not Config.UseDeferrals then
-    function setGuiState(state)
-        SetNuiFocus(state, state)
-        guiEnabled = state
+local function showIdentityForm()
+    local minDate = Config.DateFormat
+    minDate = string.gsub(minDate, '[Yy][Yy][Yy][Yy]', Config.LowestYear) -- Replace 'YYYY' or 'yyyy' with Config.LowestYear
+    minDate = string.gsub(minDate, '[Mm][Mm]', '01')                      -- Replace 'MM' or 'mm' with '01'
+    minDate = string.gsub(minDate, '[Dd][Dd]', '01')                      -- Replace 'DD' or 'dd' with '01'
 
-        if state then
-            SetTimecycleModifier(timecycleModifier)
-        else
-            ClearTimecycleModifier()
-        end
+    local maxDate = Config.DateFormat
+    maxDate = string.gsub(maxDate, '[Yy][Yy][Yy][Yy]', Config.HighestYear) -- Replace 'YYYY' or 'yyyy' with Config.HighestYear
+    maxDate = string.gsub(maxDate, '[Mm][Mm]', '01')                       -- Replace 'MM' or 'mm' with '01'
+    maxDate = string.gsub(maxDate, '[Dd][Dd]', '01')                       -- Replace 'DD' or 'dd' with '01'
 
-        SendNUIMessage({type = "enableui", enable = state})
-    end
+    local input = lib.inputDialog("IDENTITY", {
+        { type = "input",  label = "First Name",    description = "Your character's first name",    required = true, placeholder = "First Name",  min = Config.MinFirstNameLength,                max = Config.MaxFirstNameLength },
+        { type = "input",  label = "Last Name",     description = "Your character's last name",     required = true, placeholder = "Last Name",   min = Config.MinLastNameLength,                 max = Config.MaxLastNameLength },
+        { type = "date",   label = "Date Of Birth", description = "Your character's date-of-birth", required = true, format = Config.DateFormat,  min = minDate,                                  max = maxDate },
+        { type = "number", label = "Height (CM)",   description = "Your character's height",        required = true, placeholder = "Height (CM)", min = Config.MinHeight,                         max = Config.MaxHeight },
+        { type = "select", label = "Gender",        description = "Your character's gender",        required = true, placeholder = "Gender",      options = { ["m"] = "Male", ["f"] = "Female" }, default = "m" }
+    }, { allowCancel = false })
 
-    RegisterNetEvent('esx_identity:showRegisterIdentity', function()
-        TriggerEvent('esx_skin:resetFirstSpawn')
-        while not ready do
-            print('Waiting for  esx_identity NUI..')
-            Wait(100)
-        end
-        if not ESX.PlayerData.dead then setGuiState(true) end
-    end)
+    if not input then return showIdentityForm() end
 
-    RegisterNUICallback('register', function(data, cb)
-        if not guiEnabled then
-            return
-        end
+    local data = {
+        firstname = input[1],
+        lastname = input[2],
+        dateofbirth = input[3],
+        height = input[4],
+        sex = input[5]
+    }
 
-        ESX.TriggerServerCallback('esx_identity:registerIdentity', function(callback)
-            if not callback then
-                return
-            end
+    ESX.TriggerServerCallback("esx_identity:registerIdentity", function(callback)
+        if not callback then return showIdentityForm() end
 
-            ESX.ShowNotification(TranslateCap('thank_you_for_registering'))
-            setGuiState(false)
+        ESX.ShowNotification(_U("thank_you_for_registering"), "success")
 
-            if not ESX.GetConfig().Multichar then
-                TriggerEvent('esx_skin:playerRegistered')
-            end
-        end, data)
-    end)
+        if ESX.GetConfig().Multichar then return end
+
+        TriggerEvent("esx_skin:playerRegistered")
+    end, data)
 end
+
+RegisterNetEvent("esx_identity:showRegisterIdentity", function()
+    TriggerEvent("esx_skin:resetFirstSpawn")
+
+    if not ESX.PlayerData.dead then showIdentityForm() end
+end)
